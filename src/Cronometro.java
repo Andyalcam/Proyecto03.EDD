fb86bc0d93733c07888f677c9f32804a70a84787
Alfonso Mondragon <hotdogponcho@ciencias.unam.mx>
2022-01-19 21:23:39 -0600

    Actualizacion diccionario

--- src/Cronometro.java
+++ src/Cronometro.java
@@ -2,14 +2,14 @@ public class Cronometro extends Thread{
 
     @Override
     public void run(){
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 60; i++){
             try{
                 Thread.sleep(1000);
             }catch(InterruptedException ex){
                 ex.printStackTrace();
             }
         }
-
+        System.out.println("El tiempo se terminó, presiona enter.");
     }
 
 }
--- src/Juego.java
+++ src/Juego.java
@@ -1,17 +1,26 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.text.Normalizer;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Juego {
 
     private String[] diccionario;
     private List<String> secuencia;
+    private List<String> palabras;
+    private List<String> jugadores;
+    private List<String> puntuaciones;
+    private int puntuación;
     public static Cronometro cronometro = new Cronometro();
+    private Scanner in = new Scanner(System.in);
 
-    public Juego(){
+    public Juego(List<String> jugadores){
         leerDiccionario();
-        obtenerSecuencia();
-        revolver();
+        this.jugadores = jugadores;
+        palabras = new List<>();
+        puntuaciones = new List();
+        secuencia = new List<>();
     }
 
     /**
@@ -32,12 +41,6 @@ public class Juego {
         }
     }
 
-    public void setSecuencia(List<String> secuencia) {
-        if(secuencia.size() != 0)
-            secuencia.clear();
-        this.secuencia = secuencia;
-    }
-
     public String[] getDiccionario() {
         return diccionario;
     }
@@ -46,9 +49,12 @@ public class Juego {
         return secuencia;
     }
 
+    public int getPuntuación() {
+        return puntuación;
+    }
+
     private void leerDiccionario(){
         int contador = 0;
-
         try {
             FileReader fr = new FileReader("src/Diccionario.txt");
             BufferedReader br = new BufferedReader(fr);
@@ -67,17 +73,17 @@ public class Juego {
 
     public void jugar(){
 
-        Turno turno = new Turno(this);
-
         cronometro.start();
-        turno.start();
+        //turno.start();
 
         while(cronometro.isAlive()) {
-
+            Scanner in = new Scanner(System.in);
+            System.out.println("Ingresa una palabra");
+            String aux = in.nextLine();
+            validacionSec(aux);
         }
 
-        System.out.println("El tiempo se terminó, presiona enter.");
-
+        System.out.println("Puntuacion Final: " + puntuación);
 
     }
 
@@ -99,41 +105,86 @@ public class Juego {
     }
 
     public void iniciar(){
-        System.out.print("Tu secuencia de letras es: ");
-        //for (int i=0; i<secuencia.size(); i++) {
-            System.out.print(secuencia);
-        //}
+
+        for(int i = 0; i < jugadores.size(); i++){
+            System.out.println("Es turno de " + jugadores.get(i) + ". Selecciona una opción");
+            System.out.println("1. Introducir Secuencia \n2. Secuencia Aleatoria");
+            int opc = in.nextInt();
+            if(opc == 1){
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Ingresa la secuencia de 9 letras separado por comas Ej. A,B,C,D");
+                String sec = scanner.nextLine();
+                sec = sec.toUpperCase();
+                String[] arrSecuencia = sec.split(",");
+                secuencia.clear();
+                for (String letra : arrSecuencia){
+                    secuencia.add(0,letra);
+                }
+            }else if(opc == 2){
+                obtenerSecuencia();
+                revolver();
+            }else{
+                System.out.println("Fakiu");
+            }
+
+            jugar();
+
+        }
+
+
+
+
+        /*System.out.print("Tu secuencia de letras es: ");
+        System.out.print(secuencia);
         System.out.println();
-        jugar();
+        jugar();*/
     }
 
-    public void validacionSec(List<String> secuencia, String palabra){
-        palabra = palabra.toUpperCase();
-        List<String> secAux = secuencia;
+    public boolean validacionSec(String palabra){
+        String auxPalabra = cleanString(palabra);
+        auxPalabra = auxPalabra.toUpperCase();
+        List<String> secAux = new List<>();
+        for (int i = 0; i < secuencia.size(); i++) {
+            secAux.add(0,secuencia.get(i));
+        }
         boolean valida = false;
-        char[] letras =palabra.toCharArray();
+        char[] letras = auxPalabra.toCharArray();
         for(int i=0; i<letras.length; i++){
             String aux = letras[i] + "";
+            System.out.println(aux + " - " + secAux.contains(aux));
             if(secAux.contains(aux)){
                 secAux.remove(aux);
                 valida = true;
             }else{
+                System.out.println("La letra que no esta es : " + aux);
                 valida = false;
                 System.out.println("Tu palabra no es válida");
                 break;
             }
         }
-        if (valida){
+        if(valida){
             palabra = palabra.toLowerCase();
             String[] arreglito = getDiccionario();
             int indice = find(arreglito,palabra);
-            System.out.printf(indice + "");
             if(indice != -1){
-                System.out.println(" " + arreglito[indice]);
-                System.out.println("Tu palabra es válida");
+                puntuar(palabra);
+                return true;
             }else {
                 System.out.println("Tu palabra no es válida, no esta en el diccionario");
+                return false;
             }
+        }
+        return false;
+    }
+
+    private void puntuar(String palabra) {
+        if(!palabras.contains(palabra)){
+            int puntos = (int) Math.pow(palabra.length(),2);
+            puntuación += puntos;
+            palabras.add(0,palabra);
+            System.out.println("Tu palabra es válida, sumas " + puntos + " puntos.");
+        }else{
+            System.out.println("Tu palabra es válida, pero ya fue ingresada, sumas 0 puntos");
         }
     }
 
@@ -165,16 +216,22 @@ public class Juego {
         int mid = lo + ((hi - lo) / 2);
 
         if (arr[mid].compareTo(elem) == 0) {//Si el elemento es mid
-            System.out.println("mid: " + mid + ", lo: " + lo + ", hi: " + hi + ", arreglo: " + arr[mid] + " - elemento: " + elem);
+            //System.out.println("mid: " + mid + ", lo: " + lo + ", hi: " + hi + ", arreglo: " + arr[mid] + " - elemento: " + elem);
             return mid;
         }
-        if (arr[mid].compareTo(elem) < 0) { //Buscamos en la parte de la derecha
-            System.out.println("mid: " + mid + ", lo: " + lo + ", hi: " + hi + ", arreglo: " + arr[mid] + " - elemento: " + elem);
+        if (cleanString(arr[mid]).compareTo(cleanString(elem)) < 0) { //Buscamos en la parte de la derecha
+            //System.out.println("mid: " + mid + ", lo: " + lo + ", hi: " + hi + ", arreglo: " + arr[mid] + " - elemento: " + elem);
             return find(arr, elem, mid + 1, hi);
         } else { //Buscamos en la parte de la izquierda
-            System.out.println("mid: " + mid + ", lo: " + lo + ", hi: " + hi + ", arreglo: " + arr[mid] + " - elemento: " + elem);
+            //System.out.println("mid: " + mid + ", lo: " + lo + ", hi: " + hi + ", arreglo: " + arr[mid] + " - elemento: " + elem);
             return find(arr, elem, lo, mid - 1);
         }
+    }
+
+    public static String cleanString(String texto) {
+        texto = Normalizer.normalize(texto, Normalizer.Form.NFD);
+        texto = texto.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+        return texto;
     }
 
 }
